f77759ca844bb482631bcca8ad8a028f507201b1
Tolunay <tolunay894@gmail.com>
2025-04-11 12:46:13 +0300

    Updated BestProductBotItems ResponsiveDesktopDesign_1

--- src/components/bestProuduct/BestProductBotCardItems.jsx
+++ src/components/bestProuduct/BestProductBotCardItems.jsx
@@ -3,7 +3,7 @@ import BestProductBotCard from "./BestProductBotCard";
 export default function BestProductBotCardItems() {
   const images = ["images1", "images2", "images3", "images4"];
   return (
-    <div className="pb-[125px] flex flex-col gap-y-10 sm:flex-row sm:gap-x-10">
+    <div className="pb-[125px]  flex flex-col gap-y-10 sm:w-full sm:flex-row sm:flex-wrap justify-between" >
       {images.map((item, index) => (
         <BestProductBotCard key={index} ImagePath={item} />
       ))}
--- src/layouts/BestProductBotItems.jsx
+++ src/layouts/BestProductBotItems.jsx
@@ -4,7 +4,7 @@ export default function BestProductBotItems({color}) {
   return (
     <div className="flex justify-center flex-col items-center gap-y-10 sm:w-[75vw] sm:mx-auto sm:gap-y-5">
      
-       <h1 className='text-2xl font-bold mr-[625px]'>BESTSELLER PRODUCTS</h1>
+       <h1 className='text-2xl font-bold sm:w-full'>BESTSELLER PRODUCTS</h1>
        <div className="hidden border-b border-[#ECECEC] w-full sm:flex"></div>
       <BestProductBotCardItems />
     </div>
