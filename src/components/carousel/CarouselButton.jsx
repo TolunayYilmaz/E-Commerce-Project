f1f28e0ffd4271839738d64688d93ba498aa9d68
Tolunay <tolunay894@gmail.com>
2025-04-11 18:15:35 +0300

    Updated Carousels_1

---
+++ src/components/carousel/CarouselButton.jsx
@@ -0,0 +1,3 @@
+export default function CarouselButton({buttonName}){
+
+    return(<button className="w-[204px] h-[62px] bg-[#23A6F0] font-bold text-white rounded-[5px] sm:mx-10">{buttonName}</button>)}
