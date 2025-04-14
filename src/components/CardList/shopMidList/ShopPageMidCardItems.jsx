d12d39a63ec0004b57ad91e93e9f62a10fdb3703
Tolunay <tolunay894@gmail.com>
2025-04-15 00:03:20 +0300

    Updated ShopPageMidCardItems

--- src/components/CardList/shopMidList/ShopPageMidCardItems.jsx
+++ src/components/CardList/shopMidList/ShopPageMidCardItems.jsx
@@ -2,23 +2,17 @@ import ShopMidPageCard from "../../Card/ShopMidPageCard";
 
 export default function ShopPageMidCardItems({ images }) {
   return (
-<div className="flex flex-col gap-5">
-<div className="flex flex-col gap-3  items-center sm:pt-36  sm:flex-row sm:justify-between sm:w-[78%]  sm:mx-auto">
-{images.slice(0,4).map((item,index) => (
-        <ShopMidPageCard key={index} image={item} />
-      ))}
+    <div className="mx-auto">
+      <div className="hidden sm:pt-40 sm:grid  grid-cols-1  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-6 sm:gap-y-36 md:w-full max-w-screen-xl sm:px-4">
+        {images.map((item, index) => (
+          <ShopMidPageCard key={index} image={item} />
+        ))}
+      </div>
+      <div className="flex flex-col gap-3  items-center sm:hidden">
+        {images.slice(0, 4).map((item, index) => (
+          <ShopMidPageCard key={index} image={item} />
+        ))}
+      </div>
     </div>
-    <div className="hidden sm:flex flex-col gap-3  items-center sm:pt-36 sm:flex-row sm:justify-between sm:w-[78%]  sm:mx-auto">
-    {images.slice(4,8).map((item,index) => (
-        <ShopMidPageCard key={index} image={item} />
-      ))}
-    </div>
-    <div className="hidden sm:flex flex-col gap-3  items-center sm:pt-36 sm:flex-row sm:justify-between sm:w-[78%]  sm:mx-auto">
-      {images.slice(8,12).map((item,index) => (
-        <ShopMidPageCard key={index} image={item} />
-      ))}
-    </div>
-</div>
-
   );
 }
