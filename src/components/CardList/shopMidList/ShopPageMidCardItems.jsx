ceb57a6024972bff593dd5382fa4dd57c30ad664
Tolunay <tolunay894@gmail.com>
2025-04-29 22:44:19 +0300

    Updated ShopPagePath

--- src/components/CardList/shopMidList/ShopPageMidCardItems.jsx
+++ src/components/CardList/shopMidList/ShopPageMidCardItems.jsx
@@ -2,10 +2,20 @@ import ShopMidPageCard from "../../Card/ShopMidPageCard";
 import { useHistoryHook } from "../../../hooks/useHistoryHook.jsx";
 import { useParams } from "react-router-dom";
 import slugify from "slugify";
+import { useSelector } from "react-redux";
 
 export default function ShopPageMidCardItems({ products }) {
   const goToPage = useHistoryHook();
   const { gender, categoryName, categoryId } = useParams();
+  const categories = useSelector((state) => state.product.categories);
+  const path = { id: 0, gender: "", categoryName: "" };
+
+  function filterProps(item) {
+    const obj = categories.find((cat) => cat.id === item.category_id);
+    path.gender = obj.gender === "k" ? "kadin" : "erkek";
+    path.categoryName = obj.title.toLowerCase();
+    path.id = obj.id;
+  }
   return (
     <div className="mx-auto">
       <div className="hidden sm:pt-40 sm:grid  grid-cols-1  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-6 sm:gap-y-80 md:w-full max-w-screen-xl sm:px-4">
@@ -13,7 +23,16 @@ export default function ShopPageMidCardItems({ products }) {
           <ShopMidPageCard
             key={`grid-${item.id}-${index}`}
             product={item}
-            goToPage={() => goToPage(`/shop/${gender}/${categoryName}/${categoryId}/${slugify(item.name, { lower: true })}/` + item.id)}
+            goToPage={() => {
+              filterProps(item);
+              goToPage(
+                `/shop/${gender ? gender : path.gender}/${
+                  categoryName ? categoryName : path.categoryName
+                }/${categoryId ? categoryId : path.id}/${slugify(item.name, {
+                  lower: true,
+                })}/` + item.id
+              );
+            }}
           />
         ))}
       </div>
@@ -22,7 +41,16 @@ export default function ShopPageMidCardItems({ products }) {
           <ShopMidPageCard
             key={`mobile-${item.id}-${index}`}
             product={item}
-            goToPage={() => goToPage(`/shop/${gender}/${categoryName}/${categoryId}/${slugify(item.name, { lower: true })}/` + item.id)}
+            goToPage={() => {
+              filterProps(item);
+              goToPage(
+                `/shop/${gender ? gender : path.gender}/${
+                  categoryName ? categoryName : path.categoryName
+                }/${categoryId ? categoryId : path.id}/${slugify(item.name, {
+                  lower: true,
+                })}/` + item.id
+              );
+            }}
           />
         ))}
       </div>
